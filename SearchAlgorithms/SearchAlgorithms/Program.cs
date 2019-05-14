e54e8d801f4cf774d912af4f1e54901c0d1f8176
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-14 18:33:22 +0200

    Created the array

--- SearchAlgorithms/SearchAlgorithms/Program.cs
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -6,6 +6,11 @@ namespace SearchAlgorithms
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[]
+            {
+                3,8,11,17,5,8
+            };
+            int SearchValue = 11;
         }
     }
 }
