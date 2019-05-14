8d79d5f687cf203dea44910369a3e82280afd59f
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-14 18:46:52 +0200

    if else statement

--- SearchAlgorithms/SearchAlgorithms/Program.cs
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -6,11 +6,8 @@ namespace SearchAlgorithms
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[]
-            {
-                3,8,11,17,5,8
-            };
-            int SearchValue = 11;
+            int[] numbers = new int[]{ 3,8,11,17,5,8 };
+            int SearchValue = 7;
             int SearchValueIndex = -1;
 
             for (int i = 0; i < numbers.Length; i++)
@@ -22,9 +19,15 @@ namespace SearchAlgorithms
             }
 
             Console.WriteLine("We are looking for number " + SearchValue);
-            Console.WriteLine("Number found at index " + SearchValueIndex);
-
-
+            
+            if (SearchValueIndex == -1)
+            {
+                Console.WriteLine("Value was not found");
+            }
+            else
+            {
+                Console.WriteLine("Number found at index " + SearchValueIndex);
+            }
         }
     }
 }
