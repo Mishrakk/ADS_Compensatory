d78b7be5d682a1294f7fd501cb36ed3b201bcf12
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-14 19:10:00 +0200

    Improved Linear Search function

--- SearchAlgorithms/SearchAlgorithms/Program.cs
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -6,20 +6,10 @@ namespace SearchAlgorithms
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[]{ 3,8,11,17,5,8 };
+            int[] numbers = new int[] { 3, 8, 11, 17, 5, 8 };
             int SearchValue = 7;
-            int SearchValueIndex = -1;
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] == SearchValue)
-                {
-                    SearchValueIndex = i;
-                }
-            }
-
+            int SearchValueIndex = SimpleLinearSearch(numbers, SearchValue);
             Console.WriteLine("We are looking for number " + SearchValue);
-            
             if (SearchValueIndex == -1)
             {
                 Console.WriteLine("Value was not found");
@@ -29,10 +19,10 @@ namespace SearchAlgorithms
                 Console.WriteLine("Number found at index " + SearchValueIndex);
             }
         }
+
         public static int SimpleLinearSearch(int[] array, int searchValue)
         {
             int SearchValueIndex = -1;
-
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == searchValue)
@@ -41,6 +31,17 @@ namespace SearchAlgorithms
                 }
             }
             return SearchValueIndex;
+        } 
+        public static int ImprovedLinearSearch(int[] array, int searchValue)
+        {
+            for ( int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
-    }
+    } 
 }
