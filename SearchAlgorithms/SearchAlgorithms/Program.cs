1362bfd2f85f7d5f0a09c43c8fa263563a74e45c
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-14 18:40:14 +0200

    Simple linear search algorithm

--- SearchAlgorithms/SearchAlgorithms/Program.cs
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -11,6 +11,20 @@ namespace SearchAlgorithms
                 3,8,11,17,5,8
             };
             int SearchValue = 11;
+            int SearchValueIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == SearchValue)
+                {
+                    SearchValueIndex = i;
+                }
+            }
+
+            Console.WriteLine("We are looking for number " + SearchValue);
+            Console.WriteLine("Number found at index " + SearchValueIndex);
+
+
         }
     }
 }
