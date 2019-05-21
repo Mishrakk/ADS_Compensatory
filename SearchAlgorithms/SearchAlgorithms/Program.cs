f75971381fbb9df960723177f1e12c676f6320f8
Student <Student@DESKTOP-6QVR8SB>
2019-05-21 18:40:17 +0200

    RunAnalysis function

--- SearchAlgorithms/SearchAlgorithms/Program.cs
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -6,37 +6,8 @@ namespace SearchAlgorithms
     {
         static void Main(string[] args)
         {
-            int ArraySize = 100000000;
-            int SearchValue = 8;
-            int[] numbers = new int[ArraySize];
-            numbers[ArraySize - 1] = SearchValue;
-            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-            stopwatch.Start();
-            int IndexSimpleLinearSearch = SimpleLinearSearch(numbers, SearchValue);
-            stopwatch.Stop();
-            long TimeSimpleLinearSearch = stopwatch.ElapsedMilliseconds;
-            stopwatch.Reset();
-            stopwatch.Start();
-            int IndexImprovedLinearSearch = ImprovedLinearSearch(numbers, SearchValue);
-            stopwatch.Stop();
-            long TimeImprovedLinearSearch = stopwatch.ElapsedMilliseconds;
-
-            stopwatch.Reset();
-            stopwatch.Start();
-            int IndexImprovedLinearSearchWithSentinel = ImprovedLinearSearchWithSentinel(numbers, SearchValue);
-            stopwatch.Stop();
-            long TimeImprovedLinearSearchWithSentinel = stopwatch.ElapsedMilliseconds;
-            Console.WriteLine("We are looking for number " + SearchValue);
-            if (IndexSimpleLinearSearch == -1)
-            {
-                Console.WriteLine("Value was not found");
-            }
-            else
-            {
-                Console.WriteLine("Simple linear search give index {0} in time {1}ms", IndexSimpleLinearSearch, TimeSimpleLinearSearch);
-                Console.WriteLine("Improved linear search give index {0} in time {1}ms", IndexImprovedLinearSearch, TimeImprovedLinearSearch);
-                Console.WriteLine("Improved linear search with sentinel give index {0} in time {1}ms", IndexImprovedLinearSearchWithSentinel, TimeImprovedLinearSearchWithSentinel);
-            }
+            int size = 10000000;
+            RunAnalysis(size);
         }
 
         public static int SimpleLinearSearch(int[] array, int searchValue)
@@ -79,6 +50,41 @@ namespace SearchAlgorithms
             {
                 return -1;
             }
+        }
+        public static void RunAnalysis(int size)
+        {
+            
+            int SearchValue = 8;
+            int[] numbers = new int[size];
+            numbers[size - 1] = SearchValue;
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            int IndexSimpleLinearSearch = SimpleLinearSearch(numbers, SearchValue);
+            stopwatch.Stop();
+            long TimeSimpleLinearSearch = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            int IndexImprovedLinearSearch = ImprovedLinearSearch(numbers, SearchValue);
+            stopwatch.Stop();
+            long TimeImprovedLinearSearch = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            int IndexImprovedLinearSearchWithSentinel = ImprovedLinearSearchWithSentinel(numbers, SearchValue);
+            stopwatch.Stop();
+            long TimeImprovedLinearSearchWithSentinel = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine("We are looking for number " + SearchValue);
+            if (IndexSimpleLinearSearch == -1)
+            {
+                Console.WriteLine("Value was not found");
+            }
+            else
+            {
+                Console.WriteLine("Simple linear search give index {0} in time {1}ms", IndexSimpleLinearSearch, TimeSimpleLinearSearch);
+                Console.WriteLine("Improved linear search give index {0} in time {1}ms", IndexImprovedLinearSearch, TimeImprovedLinearSearch);
+                Console.WriteLine("Improved linear search with sentinel give index {0} in time {1}ms", IndexImprovedLinearSearchWithSentinel, TimeImprovedLinearSearchWithSentinel);
+            }
+        
         }
     }
 }
