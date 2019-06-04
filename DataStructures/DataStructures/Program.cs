ffda4702517fb91f7568483ff287cebe0c087e2b
Student <Student@DESKTOP-6QVR8SB>
2019-06-04 19:06:42 +0200

    Adding new node

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -8,13 +8,39 @@ namespace DataStructures
         {
         }
     }
-    class Node<T>
+    public class Node<T>
     {
         T Data;
-        Node<T> Next;
+        public Node<T> Next;
+
+        public Node(T Data)
+        {
+            this.Data = Data;
+        }
     }
-    class LinkedList<T>
+
+    public class LinkedList<T>
     {
         Node<T> First;
+
+        public LinkedList()
+        {
+            First = null;
+        }
+
+        public void Add(T value)
+        {
+            Node<T> currentNode = First;
+
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            Node<T> NewNode = new Node<T>(value);
+
+            currentNode.Next = NewNode;
+            NewNode.Next = null;
+        }
     }
 }
